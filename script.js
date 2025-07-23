17b063b7d3ddf7f4a3ff8691d4a70323dc211881
dhuruvkoli41 <dhuruvkoli41@gmail.com>
2025-07-23 13:52:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+    var typed = new Typed('#element', {
+    strings: ['<i>Wed Devloper </i>', 'Web Designer', 'Data scientist', '&amp; A Cyber security expert',],
+    typeSpeed: 50,
+                  });
